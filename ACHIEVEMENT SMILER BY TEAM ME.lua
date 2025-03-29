da3cd00074d951568cf97938b69098b6ef45cb76
arkanzulfadliputra <scriptgamingmode@gmail.com>
2025-03-29 14:14:59 +0800

    ACHIEVEMENT SMILER BY TEAM ME.lua

---
+++ ACHIEVEMENT SMILER BY TEAM ME.lua
@@ -0,0 +1,9 @@
+local achievementGiver = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Achievements/Source.lua"))()
+
+---====== Display achievement ======---
+achievementGiver({
+    Title = "Smiler",
+    Desc = "Smiler Maximum Faster Running Away",
+    Reason = "Hide Away Is Not You Died",
+    Image = "rbxassetid://12309073114"
+})
